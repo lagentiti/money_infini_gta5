1aa728a36212e6585bd25b3cebf65f9fd4f83f95
lagentiti <123580047+lagentitii@users.noreply.github.com>
2023-02-04 13:24:54 -0500

    Update index.py

--- index.py
+++ index.py
@@ -1,5 +1,7 @@
-x = 0
-y = 0
+x = 716
+y = 745
+up = 6
+click = 3
 
 import time
 import pydirectinput
@@ -8,18 +10,15 @@ import random
 
 while True:
     time.sleep(3)
-    for i in range(6):
+    for i in range(up):
         pydirectinput.press("up")
         print("Up")
         time.sleep(random.uniform(0.5, 1))
     pyautogui.moveTo(x, y, duration=0.5)
     time.sleep(1)
-    pydirectinput.mouseDown()
-    time.sleep(random.uniform(0.1, 1))
-    pydirectinput.mouseUp()
-    time.sleep(random.uniform(0.1, 1))
-    pydirectinput.mouseDown()
-    time.sleep(random.uniform(0.1, 1))
-    pydirectinput.mouseUp()
+    for i in range(click):
+        pydirectinput.mouseDown()
+        time.sleep(random.uniform(0.1, 1))
+        pydirectinput.mouseUp()
     print("click")
     time.sleep(random.uniform(40, 57))
